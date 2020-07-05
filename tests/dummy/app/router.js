fa3e657dcea64513c6ca82cd93fb7017c7ce3f75
edwarddes <edwarddes@despard-design.com>
2020-07-05 19:40:52 -0400

    ember-cli-update

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 /* eslint-disable array-callback-return */
 Router.map(function() {
@@ -15,6 +15,3 @@ Router.map(function() {
     this.route('socket-clients');
   });
 });
-/* eslint-enable */
-
-export default Router;
